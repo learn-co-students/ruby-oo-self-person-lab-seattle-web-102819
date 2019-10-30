13f57a1a92d794ef27be5b5ef18b8ef1ca3f54ec
github username <github email address>
2019-10-30 13:23:50 -0700

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,80 @@
-# your code goes here
+class Person
+
+    attr_reader :name
+    attr_writer :bank_account, :happiness #:hygiene
+    attr_accessor :hygiene
+
+    def initialize(name)
+        @name = name
+        @happiness = 8
+        @hygiene = 8
+        @bank_account = 25
+    end
+
+    def bank_account
+        @bank_account
+    end
+
+    def happiness
+        @happiness.clamp(0,10)
+    end
+
+    def hygiene
+        @hygiene.clamp(0,10)
+    end
+
+    def happy?
+        if happiness > 7
+            return true
+        else
+            return false
+        end
+    end
+
+    def clean?
+        if hygiene > 7
+            return true
+        else
+            return false
+        end
+    end
+
+    def get_paid(salary)
+        @bank_account += salary
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        if topic === "politics"
+            person.happiness -= 2
+            self.happiness -= 2
+            return "blah blah partisan blah lobbyist"
+        elsif topic === "weather"
+            person.happiness += 1
+            self.happiness += 1
+            return "blah blah sun blah rain"
+        elsif topic != "weather" or "politics"
+            return "blah blah blah blah blah"
+
+        end
+    end
+
+
+end
